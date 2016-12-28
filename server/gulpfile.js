30ee0ab13eefcc77594084b77c1a811153b755f8
grozamorei <desperatebassplayer@gmail.com>
2016-12-29 00:21:00 +0300

    added server from existing app

---
+++ server/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon');
+
+gulp.task('default', function() {
+    nodemon({
+        script: 'app.js',
+        watch: '*.js',
+        env: {NODE_ENV: 'DEVELOPMENT'}
+    })
+})
---
+++ server/request_util.js
@@ -0,0 +1,41 @@
+//
+// utility methods for routing and response writing
+
+exports.send_ok_standard = (res) => {
+    res.writeHead(200, {'Content-type': 'application/json'})
+    res.end(JSON.stringify({status: 'OK'}))
+}
+
+exports.send_ok = (res, str_data) => {
+    res.writeHead(200, {'Content-type': 'application/json'})
+    res.end(str_data)
+}
+
+exports.send_fail_standard = (res) => {
+    res.writeHead(400, {'Content-type': 'application/json'})
+    res.end(JSON.stringify({status: 'FAILED'}))
+}
+
+exports.send_fail = (res, str_data) => {
+    res.writeHead(400, {'Content-type': 'application/json'})
+    res.end(str_data)
+}
+
+exports.standartRequestHandle = (response, task, errLog, answer = null) => {
+    try {
+        const success = task()
+        if (success) {
+            if (answer === null) {
+                exports.send_ok_standard(response)
+            } else {
+                exports.send_ok(response, answer)
+            }
+        } else {
+            console.log('SOFT_ERROR: ' + errLog)
+            exports.send_fail_standard('Soft error: ' + response)
+        }
+    } catch (e) {
+        console.log('EXCEPTION: ' + errLog + "::" + e)
+        exports.send_fail_standard('Exception: ' + response)
+    }
+}
