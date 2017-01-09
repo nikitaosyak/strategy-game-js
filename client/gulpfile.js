4c5489993f5ffd6cabb26c0cd83dadaca0401ad4
grozamorei <desperatebassplayer@gmail.com>
2017-01-09 17:05:36 +0300

    fixed client's tasks (seems to be working)

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,12 +1,7 @@
-var gulp = require('gulp'),
-    hint = require('gulp-jshint'),
-    connect = require('gulp-connect'),
-    replace = require('gulp-replace'),
-    clean = require('gulp-clean'),
-    through = require('through2');
+const gulp = require('gulp')
+const connect = require('gulp-connect')
 
 gulp.task('connect', function() {
-    'use strict'
 
     connect.server({
         root: 'build/',
@@ -15,51 +10,49 @@ gulp.task('connect', function() {
     });
 });
 
-var htmlDeplist = "";
+let htmlDeps = "";
 gulp.task('collect-html-deps', function() {
     'use strict'
 
-    var isWin = /^win/.test(process.platform);
-    var d = isWin ? '\\' : '\/';
-    var regex = new RegExp('^.*src' + d + '.*\.js', 'i');
+    // const isWin = /^win/.test(process.platform);
+    // const d = isWin ? '\\' : '\/';
+    // const regex = new RegExp('^.*src' + d + '.*\.js', 'i');
+    const through = require('through2')
 
-    htmlDeplist = "";
+    htmlDeps = "";
     return gulp.src(['src/**/*.js'])
         .pipe(through.obj(function(ch, enc, cb) {
-            if (regex.test(ch.path)) {
-                var partialPath = ch.path.replace(/^.*src./i, '');
-                htmlDeplist += '\n    <script type="text/javascript" src="' + partialPath + '"></script>';    
-            }
+            // if (regex.test(ch.path)) {
+            const partialPath = ch.path.replace(/^.*src./i, '');
+            htmlDeps += '\n    <script type="text/javascript" src="' + partialPath + '"></script>';
+            // }
             cb(null, ch)
         }));
 });
 
 gulp.task('clean', function() {
-    'use strict'
-    return gulp.src('build/', {read: false})
-        .pipe(clean());    
+    const clean = require('gulp-clean')
+    return gulp.src('build/', {read: false}).pipe(clean());
 });
 
-gulp.task('deploy', ['clean', 'collect-html-deps'], function() {
-    'use strict'
+gulp.task('deploy-raw', ['clean', 'collect-html-deps'], function() {
+    const replace = require('gulp-replace')
 
     gulp.src(['src/**/*.js', '!src/lib/**/*.js'])
-        .pipe(hint())
-        .pipe(hint.reporter('default'))
         .pipe(gulp.dest('build/'));
 
     gulp.src('src/lib/**/*.js').pipe(gulp.dest('build/lib/'))
 
     gulp.src(['src/index.html'])
-        .pipe(replace(/.*<!-- GENERATED DEPS HERE -->.*/m, htmlDeplist))
+        .pipe(replace(/.*<!-- GENERATED DEPS HERE -->.*/m, htmlDeps))
         .pipe(gulp.dest('build/'));
 
     gulp.src(['src/**/*']).pipe(connect.reload());
 });
 
-gulp.task('build', ['clean', 'collect-html-deps', 'deploy'])
-
 gulp.task('watch', function() {
     'use strict'
-    gulp.watch(['src/**/*'], ['build']);
+    gulp.watch(['src/**/*'], ['deploy-raw']);
 });
+
+gulp.task('default', ['connect', 'deploy-raw', 'watch'])
