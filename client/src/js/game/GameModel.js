73c13c3d20ff6cf5bb990765ca509940f929050f
grozamorei <desperatebassplayer@gmail.com>
2017-03-05 11:54:17 +0300

    game model now contains params and layout

--- client/src/js/game/GameModel.js
+++ client/src/js/game/GameModel.js
@@ -1,5 +1,12 @@
 
-export const GameModelConstructor = () => {
+export const GameModelConstructor = (params, mapLayout) => {
+	const _state = {
+		params: params,
+		mapLayout: mapLayout
+	}
+
     return {
+    	get params() { return _state.params },
+    	get mapLayout() { return _state.mapLayout }
     }
 }
--- client/src/js/game/Session.js
+++ client/src/js/game/Session.js
@@ -2,17 +2,25 @@ import {GameModelConstructor} from './GameModel'
 import {SessionDataConstructor} from './SessionData'
 
 export const SessionConstructor = (f, sessionData) => {
-	const _state = {
-		session: SessionDataConstructor(),
-		gameParams: null,
-		mapParams: null
-	}
+    
+    const data = SessionDataConstructor()
+    data.initialize(
+        f.connection.token, 
+        sessionData.session_token, 
+        sessionData.players
+    )
 
-	_state.session.initialize(
-		f.connection.token, 
-		sessionData.session_token, 
-		sessionData.players
-	)
+    let model = null
+    f.resourceLoader.load('assets/game_params.json').then((data) => {
+        const params = data
+        f.resourceLoader.load('assets/map_11_6.json').then((data) => {
+            const mapParams = data.layout
+            model = GameModelConstructor(params, mapParams)
+        })      
+    })
 
-
+    const _state = {
+        data: data,
+        model: model,
+    }
 }
