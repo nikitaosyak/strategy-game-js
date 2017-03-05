83af92f8b227a92c0b527746cd732d9b9ca12a1e
grozamorei <desperatebassplayer@gmail.com>
2017-03-05 12:31:28 +0300

    added starting location calculating

--- client/src/js/game/Session.js
+++ client/src/js/game/Session.js
@@ -3,6 +3,8 @@ import {SessionDataConstructor} from './SessionData'
 
 export const SessionConstructor = (f, sessionData) => {
     
+    //
+    // create session data
     const data = SessionDataConstructor()
     data.initialize(
         f.connection.token, 
@@ -12,12 +14,33 @@ export const SessionConstructor = (f, sessionData) => {
     // f.connection.traceState()
     // data.traceState()
 
+    // create game model
     let model = null
-    f.resourceLoader.load('assets/game_params.json').then((data) => {
-        const params = data
-        f.resourceLoader.load('assets/map_11_6.json').then((data) => {
-            const mapParams = data.layout
-            model = GameModelConstructor(params, mapParams)
+    let myStartLocation = -1
+    f.resourceLoader.load('assets/game_params.json').then((params) => {
+        f.resourceLoader.load('assets/map_11_6.json').then((layout) => {
+            layout = layout.layout
+            
+            //
+            // determine self start location (tile-wise)
+            // start location tiles is coded with '1' tile type
+            if (f.connection.isOnline()) {
+                let currentPointIndex = 0
+                for (let i = 0; i < layout.length; i++) {
+                    if (layout[i] !== 1) continue
+                    if (data.myIndex === currentPointIndex) {
+                        myStartLocation = i
+                        break
+                    } else {
+                        currentPointIndex += 1
+                    }
+                }
+            } else {
+                myStartLocation = 50 // we dont give a fuck in offline
+            }
+            console.log('session: my location: ' + myStartLocation)
+
+            model = GameModelConstructor(params, layout)
         })      
     })
 
