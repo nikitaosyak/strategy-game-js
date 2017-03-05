80db83d80b909d1a6f6ac79499aced97667da5ed
grozamorei <desperatebassplayer@gmail.com>
2017-03-05 11:13:56 +0300

    removed game model from facade

--- client/src/js/Facade.js
+++ client/src/js/Facade.js
@@ -1,23 +1,20 @@
 import {ThreeJsRendererConstructor} from "./render/ThreeJsRenderer";
 import {ResourceLoaderConstructor} from "./util/ResourceLoader";
-import {ModelConstructor} from './game/Model'
 import {ConnectionConstructor} from './Connection'
 
 /**
- * @returns {{connection, model, renderer, resourceLoader}}
+ * @returns {{connection, renderer, resourceLoader}}
  * @constructor
  */
 export const FacadeConstructor = () => {
     const _state = {
         resourceLoader: ResourceLoaderConstructor(),
         connection: ConnectionConstructor('localhost', 8181),
-        model: ModelConstructor(),
         renderer: ThreeJsRendererConstructor()
     }
 
     return {
         get connection() { return _state.connection},
-        get model() { return _state.model },
         get renderer() { return _state.renderer },
         get resourceLoader() { return _state.resourceLoader }
     }
