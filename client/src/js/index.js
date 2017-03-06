f907b536d892b9f6e470278cf143a88c4e464483
grozamorei <desperatebassplayer@gmail.com>
2017-03-06 12:28:34 +0300

    reorganized entry point for better reading

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,4 @@
 import {FacadeConstructor} from './Facade'
-import {HexagonConstructor} from './game/Hexagon'
 import {SessionConstructor} from './game/Session'
 
 //
@@ -8,19 +7,16 @@ const f = window.facade = FacadeConstructor()
 f.renderer.init()
 f.renderer.getCamera().position.z = 4
 
-//
-// initialize connection with server
-f.connection.connect().then(() => {
-    // connection made, waiting session begin
-    console.log('main: uplink estableashed. Token: %s', f.connection.token)
-    f.connection.enqueue().then((sessionData) => {
-        // game starting here
-        console.log('main: session linked')
-        SessionConstructor(sessionData)
-    }, () => { throw 'Cannot enqueue' })
-}, () => {
-    // game starts in offline mode
-    SessionConstructor(f, 
-        {session_token:'DEADBEEF', users:['DEADBEEF']}
-        )
-})
+f.connection.connect().then(
+    () => {
+        console.log('main: uplink estableashed. Token: %s', f.connection.token)
+        f.connection.enqueue().then(
+            SessionConstructor,
+            () => {
+                throw 'Retry queue not Implemented'
+            })
+    },
+    () => {
+        console.log('main: uplink not possible. starting OFFLINE mode')
+        SessionConstructor({session_token:'DEADBEEF', users:['DEADBEEF']})
+    })
