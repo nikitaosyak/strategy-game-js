001b989774672098165ef77c0e88b4dfc458dadb
grozamorei <desperatebassplayer@gmail.com>
2017-04-04 10:35:27 +0300

    moved intersector method to common behaviours

--- client/src/js/game/input/Intersector.js
+++ client/src/js/game/input/Intersector.js
@@ -1,31 +1,18 @@
+import {intersectSelectionTest} from "../../util/behaviours";
 
 export const IntersectorConstructor = (camera) => {
 
     const _state = {
-        selected: Number.NaN
+        selected: Number.NaN,
+        camera: camera
     }
 
-    const raycaster = new THREE.Raycaster()
-    const pointerVec = new THREE.Vector2()
-
-    return {
+    const self = {
         get anySelected() { return !Number.isNaN(_state.selected) },
         get selection() { return _state.selected },
-        update: () => { _state.selected = Number.NaN },
-        test: (objects, pointer, domElement) => {
-            pointerVec.x = (pointer.x / domElement.width) * 2 - 1
-            pointerVec.y = -(pointer.y / domElement.height) * 2 + 1
-            // console.log(pointerVec, objects)
-
-            raycaster.setFromCamera(pointerVec, camera)
-
-            const result = raycaster.intersectObjects(objects)
-            if (result.length > 0) {
-                _state.selected = result[0].object.gameIndex
-                // console.log(result[0].object.material.color.setHex(0xFF0000), result[0].object.gameIndex)
-            } else {
-                _state.selected = Number.NaN
-            }
-        }
+        update: () => { _state.selected = Number.NaN }
     }
+
+    Object.assign(self, intersectSelectionTest(_state))
+    return self
 }
--- client/src/js/util/behaviours.js
+++ client/src/js/util/behaviours.js
@@ -25,3 +25,23 @@ export const visualLoader = (state) => ({loadVisual: () => {
             }, reject)
         })
 }})
+
+export const intersectSelectionTest = (state) => {
+    const _raycaster = new THREE.Raycaster()
+    const _vectorPointer = new THREE.Vector2()
+    return {
+        test: (objects, pointer, domElement) => {
+            _vectorPointer.x = (pointer.x / domElement.width) * 2 - 1
+            _vectorPointer.y = -(pointer.y / domElement.height) * 2 + 1
+
+            _raycaster.setFromCamera(_vectorPointer, state.camera)
+
+            const result = _raycaster.intersectObjects(objects)
+            if (result.length > 0) {
+                state.selected = result[0].object.gameIndex
+            } else {
+                state.selected = Number.NaN
+            }
+        }
+    }
+}
